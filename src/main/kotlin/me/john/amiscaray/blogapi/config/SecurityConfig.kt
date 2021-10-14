83dc84995c13cdc0f48415da1f2f562f29b19889
john-amiscaray <john_ernest_amiscaray@outlook.com>
2021-10-14 13:41:13 -0400

    Made GET requests to the blog post controller public.

--- src/main/kotlin/me/john/amiscaray/blogapi/config/SecurityConfig.kt
+++ src/main/kotlin/me/john/amiscaray/blogapi/config/SecurityConfig.kt
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
@@ -52,6 +53,7 @@ class SecurityConfig(private val userDetailsService: UserDetailsService,
             .antMatchers("/webjars/**")
             .antMatchers("/swagger-resources/**")
             .antMatchers("/views/**")
+            .antMatchers(HttpMethod.GET, "/blog/**")
     }
 
     @Throws(java.lang.Exception::class)
